3bf6e5c77bcacea6565664b0322202ef0e1055da
josueemg <josueomega355@gmail.com>
2021-04-22 23:20:23 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ const socketIO = require('socket.io')
 const morgan = require('morgan')
 
 app.set('port', process.env.PORT || 3000)
-app.set('view engine', '.ejs')
-app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', '.ejs')
+// app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    res.render('index')
-})
+//     res.redirect('index.html')
+// })
 
 const server = app.listen(app.get('port'), () => {
 
